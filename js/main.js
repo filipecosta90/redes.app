1bd57da97a6388661b92efce106789d55bd7970f
filipecosta90 <filipecosta.90@gmail.com>
2015-11-16 15:29:46 +0000

    corrigidos paths, separados js e html

--- js/main.js
+++ js/main.js
@@ -1,216 +1,189 @@
 /**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * AngularJS Network Managment Assignment1
+ * @author Filipe Oliveira <a57816@alunos.uminho.pt>
  */
 
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('networkManagmentWebApp', [
-		'ngRoute'
-		]);
-
+var app = angular.module('networkManagmentWebApp', ['ngRoute','nvd3ChartDirectives']);
 
 /**
  * Configure the Routes
  */
 app.config(['$routeProvider', function ($routeProvider) {
-		$routeProvider
-		// Home
-		.when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-		// Pages
-		.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-		.when("/chart", {templateUrl: "partials/chart.html", controller: "PageCtrl"})
-		.when("/snmp", {templateUrl: "partials/snmp.html", controller: "PageCtrl"})
-		.when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-		.when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-		// Blog
-		.when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-		.when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
-		// else 404
-		.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
-		}]);
+  $routeProvider
+  // Home
+  .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+  // Pages
+  .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+  .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+  // else 404
+  .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
 
 /**
  * Controls the SNMP PHP REQUEST
  */
-app.controller('SNMP', function($scope, $http) {	
-		console.log(" SNMP Controller reporting for duty.");
-		$scope.url = 'php/snmp_redes.php'; // The url of our search
 
-		// The function that will be executed on button click (ng-click="search()")
-		$scope.search = function() {
+app.controller('snmp_controller', function($scope,  $http,  $log, $interval){
+  console.log(" Formulario SNMP Controller reporting for duty.");
+  $scope.submission = false;
+  $scope.formData = {};
+  $scope.dataCounter = 0;
+  $scope.keys = [];
+  $scope.graphData = [];
+  $scope.initial_measured = false;
+  $scope.measured_time = 0;
+  $scope.measured_value = 0;
 
-		// Create the http post request
-		// the data holds the keywords
-		// The request is a JSON request.
-		$http.post($scope.url, { "data" : $scope.keywords}).
-		success(function(data, status) {
-			$scope.status = status;
-			$scope.data = data;
-			$scope.result = data; // Show result from server in our <pre></pre> element
-			})
-		.
-		error(function(data, status) {
-			$scope.data = data || "Request failed";
-			$scope.status = status;			
-			});
-		};
-});
+  $scope.calculateKeys = function () {
+    $scope.keys.push("foo");
+    $scope.keys.push("bar");
+    $scope.graphData.push({ "key": "foo", "values": [] });
+    $scope.graphData.push({ "key": "bar", "values": [] });
+  };
 
-app.controller('SNMP_FORM', function($scope, $http){
-		console.log(" Formulario SNMP Controller reporting for duty.");
-		$scope.submission = false;
+  $scope.dataCounter = 0;
 
-		$scope.formData = {};
-		// submission message doesn't show when page loads
+  $scope.graphs = [
+  {
+    "name": "Foos Only",
+      "height": 300,
+      "series": [ { label: 'Foo', key: 'foo', enabled: true } ]
+  },
+  {
+    "name": "Bars Only",
+    "height": 300,
+    "series": [ { label: 'Bar', key: 'bar', enabled: true } ]
+  }
+];
 
-		// Updated code thanks to Yotam
-		var param = function(data) {
-		var returnString = '';
-		for (d in data){
-		if (data.hasOwnProperty(d))
-		returnString += d + '=' + data[d] + '&';
-		}
-		// Remove last ampersand and return
-		return returnString.slice( 0, returnString.length - 1 );
-		};
-		$scope.submitForm = function() {
-		console.log(" Form submited.");
-		$http({
-method : 'POST',
-url : 'php/snmpv2.php',
-data : param($scope.formData), // pass in data as strings
-headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
-})
-.success(function(data) {
-		if (!data.success) {
-		// if not successful, bind errors to error variables
-		$scope.error_ip_address = data.errors.ip_address;
-		$scope.error_snmp_key = data.errors.snmp_key;
-		$scope.error_snmp_mib = data.errors.snmp_mib;
-		$scope.submissionMessage = data.messageError;
-		$scope.submission = true; //shows the error message
-		} else {
-		// if successful, bind success message to message
-                        $scope.data = data;
-                        $scope.submission_result = data; // Show result from server in our <pre></pre> element
-		
-$scope.submissionMessage = data.messageSuccess;
-		$scope.formData = {}; // form fields are emptied with this line
-		$scope.submission = true; //shows the success message
-		}
-		});
+var param = function(data) {
+  var returnString = '';
+  for (d in data){
+    if (data.hasOwnProperty(d))
+      returnString += d + '=' + data[d] + '&';
+  }
+  // Remove last ampersand and return
+  return returnString.slice( 0, returnString.length - 1 );
 };
-});
 
-  app.controller('TicksCtrl', ['$scope' , '$interval', function ($scope, $interval) {
-    
-                console.log("Ticks Controller reporting for duty.");
-var maximum = document.getElementById('container').clientWidth / 2 || 300;
-    $scope.data = [[]];
-    $scope.labels = [];
-    $scope.options = {
-      animation: false,
-      showScale: false,
-      showTooltips: false,
-      pointDot: false,
-      datasetStrokeWidth: 0.5
-    };
+$scope.fetchSNMP = function() {
+  $http({
+    method : 'POST',
+    url : 'php/snmpv2.php',
+    data : param($scope.formData), // pass in data as strings
+    headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+  })
+  .success(function(data) {
+    if (!data.success) {
+      // if not successful, bind errors to error variables
+      $scope.error_ip_address = data.errors.ip_address;
+      $scope.error_snmp_key = data.errors.snmp_key;
+      $scope.error_snmp_mib = data.errors.snmp_mib;
+      $scope.submissionMessage = data.messageError;
+      $scope.submission = true; //shows the error message
+    } else {
+      // if successful, bind success message to message
 
-    // Update the dataset at 25FPS for a smoothly-animating chart
-    $interval(function () {
-      getLiveChartData();
-    }, 40);
-
-    function getLiveChartData () {
-      if ($scope.data[0].length) {
-        $scope.labels = $scope.labels.slice(1);
-        $scope.data[0] = $scope.data[0].slice(1);
+      if ($scope.keys.length === 0) {
+        $scope.calculateKeys();
+      }
+      if ($scope.initial_measured == true) {
+        $scope.measured_time = angular.fromJson(data.time);;
       }
 
-      while ($scope.data[0].length < maximum) {
-        $scope.labels.push('');
-        $scope.data[0].push(getRandomValue($scope.data[0]));
+      $scope.data = data;
+      var tempo = angular.fromJson(data.time);
+      var snmp_packets = angular.fromJson (data.snmp_packets);
+      console.log(" Tempo: "+tempo + " snmp packets: " + snmp_packets );
+      var timeDiff = tempo - $scope.measured_time;
+      $scope.dataCounter = $scope.dataCounter + timeDiff;
+      $scope.measured_time = tempo;
+      console.log('setInterval counter is now at : ' + $scope.dataCounter);
+      $scope.graphData[0].values.push([$scope.dataCounter, snmp_packets])
+        $scope.submission_result = data; // Show result from server in our <pre></pre> element
+      $scope.submissionMessage = data.messageSuccess;
+      $scope.submission = true; //shows the success message
+    }
+  });
+};
+
+var stop;
+
+$scope.clear = function(){
+  $scope.dataCounter = 0;
+  $scope.keys = [];
+  $scope.graphData = [];
+
+  $scope.calculateKeys();
+};
+
+$scope.start = function() {
+  // Don't start again
+  if ( angular.isDefined(stop) ) return;
+  $scope.fetchSNMP();
+  if ( $scope.submission == true ){
+    stop = $interval(function() {
+      $scope.fetchSNMP();
+    }, 1000);
+  }	
+};
+
+$scope.stop = function() {
+  if (angular.isDefined(stop)) {
+    $interval.cancel(stop);
+    stop = undefined;
+  }
+};
+
+$scope.$on('$destroy', function() {
+  // Make sure that the interval is destroyed too
+  $scope.stop();
+});
+
+});
+
+app.filter('graphDataFilter', function () {
+  return function (data, series) {
+    var r = [];
+
+    for (var s = 0; s < series.length; s++) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].key == series[s].key) {
+          r.push({ key: series[s].label, values: data[i].values, disabled: !series[s].enabled });
+        }
       }
     }
 
-  function getRandomValue (data) {
-    var l = data.length, previous = l ? data[l - 1] : 50;
-    var y = previous + Math.random() * 10 - 5;
-    return y < 0 ? 0 : y > 100 ? 100 : y;
-  }
-
-  }]);
-
-app.controller('TabsCtrl', function ($scope) {
-    $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    $scope.active = true;
-    $scope.data = [
-      [65, 59, 90, 81, 56, 55, 40],
-      [28, 48, 40, 19, 96, 27, 100]
-    ];
-  });
-
-  app.controller('DataTablesCtrl', function ($scope) {
-                console.log("DataTables Controller reporting for duty.");
-    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-    $scope.colours = [
-      { // grey
-        fillColor: 'rgba(148,159,177,0.2)',
-        strokeColor: 'rgba(148,159,177,1)',
-        pointColor: 'rgba(148,159,177,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(148,159,177,0.8)'
-      },
-      { // dark grey
-        fillColor: 'rgba(77,83,96,0.2)',
-        strokeColor: 'rgba(77,83,96,1)',
-        pointColor: 'rgba(77,83,96,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(77,83,96,1)'
-      }
-    ];
-    $scope.randomize = function () {
-      $scope.data = $scope.data.map(function (data) {
-        return data.map(function (y) {
-          y = y + Math.random() * 10 - 5;
-          return parseInt(y < 0 ? 0 : y > 100 ? 100 : y);
-        });
-      });
-    };
-  });
-
-
-/**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-		console.log("Blog Controller reporting for duty.");
-		});
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-		console.log("Page Controller reporting for duty.");
-
-		// Activates the Carousel
-		$('.carousel').carousel({
-interval: 5000
-});
-
-		// Activates Tooltips for Social Links
-		$('.tooltip-social').tooltip({
-selector: "a[data-toggle=tooltip]"
+    return r;
+  };
 })
-		});
+
+app.directive('extendedChart', function () {
+  return {
+    restrict: 'E',
+  link: function ($scope) {
+    $scope.d3Call = function (data, chart) {
+      var svg = d3.select('#' + $scope.id + ' svg').datum(data);
+
+      var path = svg.selectAll('path');
+
+      path.data(data)
+  .transition()
+  .ease("linear")
+  .duration(300);
+
+return svg.transition()
+  .duration(300)
+  .call(chart);
+    };
+  }
+  };
+})
+
+});
 
 
 
--- php/snmpv2.php
+++ php/snmpv2.php
@@ -9,44 +9,41 @@ $errors = array(); //array to hold errors
 
 
 if (empty($_POST['ip_address']))
-$errors['ip_address'] = 'ipAddress is required.';
+  $errors['ip_address'] = 'ipAddress is required.';
 if (empty($_POST['snmp_key']))
-$errors['snmp_key'] = 'SNMP Key is required.';
+  $errors['snmp_key'] = 'SNMP Key is required.';
 if (empty($_POST['snmp_mib']))
-$errors['snmp_mib'] = 'SNMP MIB  is required.';
+  $errors['snmp_mib'] = 'SNMP MIB  is required.';
 if (empty($_POST['snmp_mib_time']))
-$errors['snmp_mib_time'] = 'SNMP MIB for time tracking is required.';
+  $errors['snmp_mib_time'] = 'SNMP MIB for time tracking is required.';
 // return a response ===========================================================
 // response if there are errors
 
 if ( ! empty($errors)) {
-	// if there are items in our errors array, return those errors
-	$data['success'] = false;
-	$data['errors'] = $errors;
-	$data['messageError'] = 'Please check the fields in red';
+  // if there are items in our errors array, return those errors
+  $data['success'] = false;
+  $data['errors'] = $errors;
+  $data['messageError'] = 'Please check the fields in red';
 } else {
-	// if there are no errors, return a message
-	$ip_address = $_POST['ip_address']; // required
-	$snmp_key = $_POST['snmp_key']; // required
-	$snmp_mib = $_POST['snmp_mib']; // required
-	$snmp_mib_time = $_POST['snmp_mib_time']; // required
+  // if there are no errors, return a message
+  $ip_address = $_POST['ip_address']; // required
+  $snmp_key = $_POST['snmp_key']; // required
+  $snmp_mib = $_POST['snmp_mib']; // required
+  $snmp_mib_time = $_POST['snmp_mib_time']; // required
 
-	$time = snmp2_get($ip_address, $snmp_key , "SNMPv2-MIB::sysUpTime.0");
-	$token = strtok($time, " ()\n\t");
-	$token_time = strtok(" ()\n\t");
+  $time = snmp2_get($ip_address, $snmp_key , "SNMPv2-MIB::sysUpTime.0");
+  $token = strtok($time, " ()\n\t");
+  $token_time = strtok(" ()\n\t");
 
-	$values = snmp2_getnext($ip_address, $snmp_key, "1.3.6.1.2.1.4.3");
-	$data['time'] = $token_time;
-	$token_packets = strtok($values , " : \n\t");
-	$token_packets = strtok ( " : \n\t");
-	$data['snmp_packets']= $token_packets; 
-	$data['success'] = true;
-	$data['messageSuccess'] = 'SNMP command executed';
+  $values = snmp2_getnext($ip_address, $snmp_key, "1.3.6.1.2.1.4.3");
+  $data['time'] = $token_time;
+  $token_packets = strtok($values , " : \n\t");
+  $token_packets = strtok ( " : \n\t");
+  $data['snmp_packets']= $token_packets; 
+  $data['success'] = true;
+  $data['messageSuccess'] = 'SNMP command executed';
 
 }
 // return all our data to an AJAX call
 echo json_encode($data);
-
-
-
 

361d4c4f3b7c7d3028c781196254e754987323b8
filipecosta90 <filipecosta.90@gmail.com>
2015-11-21 02:11:02 +0000

    snmp bulk almost runnning

--- perl/snmp.pl
+++ perl/snmp.pl
@@ -9,28 +9,34 @@ my $query = CGI->new;
 
 my %data;
 my %errors;
+my $response;
+
+my $ip_address;
+my $snmp_key;
+my @snmp_oids;
+my $snmp_mib_time;
 
 if ( defined( $query->param('ip_address') ) ) {
-	my $ip_address = $query->param('ip_address');
+	$ip_address = $query->param('ip_address');
 }	
 else {
 	$errors{'ip_address'} = 'ipAddress is required.';
 }
 
 if ( defined( $query->param('snmp_key') ) ) {
-	my $snmp_key = $query->param('snmp_key');
+	$snmp_key = $query->param('snmp_key');
 }	
 else {
 	$errors{'snmp_key'} = 'SNMP Key is required.';
 }
-if ( defined( $query->param('snmp_mib') ) ) {
-	my @snmp_mib = $query->param('snmp_mib');
+if ( defined( $query->param('snmp_oids') ) ) {
+	@snmp_oids = $query->param('snmp_oids');
 }	
 else {
-	$errors{'snmp_mib'} = 'SNMP MIB is required.';
+	$errors{'snmp_oids'} = 'At least one SNMP OID is required.';
 }
 if ( defined( $query->param('snmp_mib_time') ) ) {
-	my $snmp_mib_time = $query->param('snmp_mib_time');
+	$snmp_mib_time = $query->param('snmp_mib_time');
 }	
 else {
 	$errors{'snmp_mib_time'} = 'SNMP MIB for time tracking is required.';
@@ -40,9 +46,40 @@ if ( %errors ) {
 	$data{'success'} = 'false';
 	$data{'errors'} = \%errors;
 	$data{'messageError'} = 'Please check the fields in red';	
+	my $json_response = encode_json( { %data } );
+	print $query->header("application/json");
+	print $json_response;
+
 } 
+else {
+	my ($session, $error) = Net::SNMP->session(
+			-hostname => $ip_address,
+			-version => 'snmpv2c',
+			-community => $snmp_key
+			);
 
-my $response = encode_json( { %data } );
+	if (!defined $session) {
+		$data{'success'} = 'false';
+		$errors{'session'} = 'Unable to define Net::SNMP session.';
+		$data{'errors'} = \%errors;
+		$data{'messageError'} = 'Please check the fields in red';
+		my $json_response = encode_json( { %data } );
+		print $query->header("application/json");
+		print $json_response;
 
-print $query->header("application/json");
-print $response;
+
+
+	}
+	else{
+		$response = $session->get_bulk_request (
+				-nonrepeaters => '1',
+				-maxrepetitions => scalar @snmp_oids,
+				-varbindlist     => \@snmp_oids
+				);
+		$session->close();
+		my $json_response = encode_json( { $response } );
+		print $query->header("application/json");
+		print $json_response;
+	}
+}
+

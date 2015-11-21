bc3019ef344cfbb4094e6df8fff8e66efd2539f7
filipecosta90 <filipecosta.90@gmail.com>
2015-11-21 22:13:51 +0000

    perl and snmp running on server. finallygit add --allgit add --all

--- cgi-bin/snmp.pl
+++ cgi-bin/snmp.pl
@@ -4,7 +4,7 @@ use warnings;
 
 use Data::Dumper;
 use JSON;
-use CGI;
+use CGI qw(:standard);
 use CGI::Carp 'fatalsToBrowser';
 use Net::SNMP qw(:snmp);
 use DB;
@@ -13,18 +13,14 @@ my $query = CGI->new;
 
 my %data;
 my %errors;
-my @response;
 
 my $ip_address;
 my $snmp_key;
-#1.3.6.1.2.1.4.3;
 my @snmp_oids;
-#1.3.6.1.2.1.1.3;
-my $snmp_mib_time;
 my $session;
 my $error;
 
-my $sysUpTime = '1.3.6.1.2.1.1.3.0';
+my $sysUpTime = '1.3.6.1.2.1.1.3';
 
 if ( defined( $query->param('ip_address') ) ) {
 	$ip_address = $query->param('ip_address');
@@ -50,10 +46,6 @@ if ( %errors ) {
 	$data{'success'} = 'false';
 	$data{'errors'} = \%errors;
 	$data{'messageError'} = 'Please check the fields in red';
-	my $json_response = encode_json( { %data } );
-	print $query->header("application/json");
-	print $json_response;
-
 }
 else {
 	($session, $error) = Net::SNMP->session(
@@ -70,13 +62,10 @@ else {
 		$errors{'session'} = 'Unable to define Net::SNMP session.';
 		$data{'errors'} = \%errors;
 		$data{'messageError'} = 'Please check the fields in red';
-		my $json_response = encode_json( { %data } );
-		print $query->header("application/json");
-		print $json_response;
-
 	}
 	else{
-		my @args;
+ $data{'success'} = 'true';		
+my @args;
 		my @snmp_varbind;	
 
 		push (@snmp_varbind , $sysUpTime);	
@@ -86,19 +75,24 @@ else {
 		push (@args, -maxrepetitions => 1  ); 
 
 		$session->get_bulk_request(@args);
-		print $query->header("application/json");
-		my @oids = oid_lex_sort(keys(%{$session->var_bind_list()}));
+		my %oids = oid_lex_sort(keys(%{$session->var_bind_list()}));
 
-		my $uptime = $session->var_bind_list->{$sysUpTime};
-		print encode_json( { "snmp_mib_time" => "$uptime"} );
-		delete @oids->{$sysUpTime};
-
-		foreach (@oids) {
+		my $uptime = $session->var_bind_list->{"$sysUpTime.0"};
+		$data{'snmp_time'} = $uptime;
+		foreach (%oids) {
 			my $oid_key = $_;
 			my $response;
 			my $string = $session->var_bind_list()->{$_};
-		$response = encode_json( { "$oid_key" => "$string" });
-			print $response;
+			if ( $oid_key eq "$sysUpTime.0"){
+
+			}
+			else{
+				$data{'snmp_data'} = $string;
+			}
 		}
 	}
 }
+
+print header('application/json');
+my $json_response = encode_json( { %data } );
+print $json_response;
